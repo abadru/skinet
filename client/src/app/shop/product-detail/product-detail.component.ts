cf95fafeff4bd9a4fbc4b8404dda6ac1154e1775
Abdul Badru <abdul.badru@gmail.com>
2020-05-19 19:29:11 +0200

    End of Section 10

---
+++ client/src/app/shop/product-detail/product-detail.component.ts
@@ -0,0 +1,27 @@
+import { Component, OnInit } from '@angular/core';
+import {IProduct} from '../../shared/models/product';
+import {ShopService} from '../shop.service';
+import {ActivatedRoute} from '@angular/router';
+
+@Component({
+  selector: 'app-product-detail',
+  templateUrl: './product-detail.component.html',
+  styleUrls: ['./product-detail.component.scss']
+})
+export class ProductDetailComponent implements OnInit {
+  product: IProduct;
+  constructor(private shopService: ShopService, private activatedRoute: ActivatedRoute) { }
+
+  ngOnInit(): void {
+    this.getProduct();
+  }
+
+  getProduct() {
+    this.shopService.getProduct(+this.activatedRoute.snapshot.paramMap.get('id')).subscribe(response => {
+      this.product = response;
+    }, error => {
+      console.log(error);
+    });
+  }
+
+}
--- client/src/app/shop/shop.module.ts
+++ client/src/app/shop/shop.module.ts
@@ -3,17 +3,19 @@ import { CommonModule } from '@angular/common';
 import { ShopComponent } from './shop.component';
 import { ProductItemComponent } from './product-item/product-item.component';
 import {SharedModule} from '../shared/shared.module';
+import { ProductDetailComponent } from './product-detail/product-detail.component';
+import {ShopRoutingModule} from './shop-routing.module';
 
 
 
 @NgModule({
-  declarations: [ShopComponent, ProductItemComponent],
+  declarations: [ShopComponent, ProductItemComponent, ProductDetailComponent],
   exports: [
-    ShopComponent,
   ],
   imports: [
     CommonModule,
-    SharedModule
+    SharedModule,
+    ShopRoutingModule
   ],
 })
 export class ShopModule { }
