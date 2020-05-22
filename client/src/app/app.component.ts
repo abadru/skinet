3112225c006056e428f60691c6bbb8d835520248
Abdul Badru <abdul.badru@gmail.com>
2020-05-22 22:38:19 +0200

    End of Section 17

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,5 +1,6 @@
 import {Component, OnInit} from '@angular/core';
 import {BasketService} from './basket/basket.service';
+import {AccountService} from './account/account.service';
 
 @Component({
   selector: 'app-root',
@@ -7,9 +8,14 @@ import {BasketService} from './basket/basket.service';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent implements OnInit {
-  constructor(private basketService: BasketService) {}
+  constructor(private basketService: BasketService, private accountService: AccountService) {}
 
   ngOnInit(): void {
+   this.loadBasket();
+   this.loadCurrentUser();
+  }
+
+  loadBasket() {
     const basketId = localStorage.getItem('basket_id');
     if (basketId) {
       this.basketService.getBasket(basketId)
@@ -18,5 +24,15 @@ export class AppComponent implements OnInit {
             console.log(error);
           });
     }
+  }
+
+  loadCurrentUser() {
+    const token = localStorage.getItem('token');
+    this.accountService.loadCurrentUser(token).
+      subscribe(response => console.log('Loaded User'),
+        error => {
+          console.log(error);
+        });
+
   }
 }
